56f68e5f4213fd503dda7d1b39566002fbc64fe1
lgusermamie@gmail.com <lgusermamie@gmail.com>
2026-05-21 09:54:42 +0000

    stg_nb_fer 2023 s1/s2

--- models/transports_IDFM/staging/stg_nb_fer_2023_s2.sql
+++ models/transports_IDFM/staging/stg_nb_fer_2023_s2.sql
@@ -3,11 +3,11 @@ JOUR as date
 cast(code_stif_trns as string) as code_stif_trns,
 cast(code_stif_res as string) as code_stif_res,
 cast(libelle_arret as string) as libelle_arret,
-cast(lda as string) as id_stop_idfm,
+cast(id_zdc as string) as id_stop_idfm,
 cast(categorie_titre as string),
 cast (nb_valid as int)
 
-from {{source('idfm','nb_fer_2023_s1')}}
+from {{source('idfm','nb_fer_2023_s2')}}
 
 
 row_number() over (
@@ -17,7 +17,7 @@ row_number() over (
                 CATEGORIE_TITRE
         ) as rn
 
-    from {{ source('idfm', 'nb_fer_2023_s1') }}
+    from {{ source('idfm', 'nb_fer_2023_s2') }}
 
     where JOUR is not null
       and CODE_STIF_ARRET is not null
