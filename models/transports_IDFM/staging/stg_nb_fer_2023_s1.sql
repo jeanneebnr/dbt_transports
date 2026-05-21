7dc3ea9e98a5926fb9b42c67ca3e93adccb22d28
lgusermamie@gmail.com <lgusermamie@gmail.com>
2026-05-21 10:37:36 +0000

    stg_nb_fer_2024 s1/t3

--- models/transports_IDFM/staging/stg_nb_fer_2023_s1.sql
+++ models/transports_IDFM/staging/stg_nb_fer_2023_s1.sql
@@ -1,27 +1,13 @@
-select
-JOUR as date
-cast(code_stif_trns as string) as code_stif_trns,
-cast(code_stif_res as string) as code_stif_res,
-cast(libelle_arret as string) as libelle_arret,
-cast(lda as string) as id_stop_idfm,
-cast(categorie_titre as string),
-cast (nb_valid as int)
+  select
+    JOUR as date,
+    cast(code_stif_trns as string) as id_transporteur_stif,
+    cast(code_stif_res as string) as id_reseau_stif,
+    cast(libelle_arret as string) as libelle_arret,
+    cast(lda as string) as id_stop_idfm,
+    cast(categorie_titre as string) as categorie_titre,
+    cast(nb_vald as int64) as nb_vald
 
-from {{source('idfm','nb_fer_2023_s1')}}
+from {{ source('idfm', 'nb_fer_2023_s1') }}
 
 
-row_number() over (
-            partition by
-                JOUR,
-                CODE_STIF_ARRET,
-                CATEGORIE_TITRE
-        ) as rn
 
-    from {{ source('idfm', 'nb_fer_2023_s1') }}
-
-    where JOUR is not null
-      and CODE_STIF_ARRET is not null
-      and NB_VALD is not null
-)
-
-where rn = 1
