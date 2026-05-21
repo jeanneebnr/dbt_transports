f053409ee1012a02c000805a0b620ae8e6fec215
RUDY TRIPIER <rudy.tripier@student.lacapsule.academy>
2026-05-21 09:09:54 +0000

    avancé rudy

--- models/transports_IDFM/staging/stg_indicateurs_de_perception_qs.sql
+++ models/transports_IDFM/staging/stg_indicateurs_de_perception_qs.sql
@@ -1,2 +1,7 @@
-select *
-from {{source('idfm', 'indicateurs_de_perception_qs')}}
+SELECT
+    * EXCEPT(operateur, mode, groupe_de_lignes, ligne),
+    operateur        AS transporteur_name,
+    mode             AS type_transport,
+    groupe_de_lignes AS libelle_groupe_de_lignes,
+    ligne            AS libelle_ligne
+FROM {{ source('idfm', 'indicateurs_de_perception_qs') }}
--- models/transports_IDFM/staging/stg_liste_transporteurs.sql
+++ models/transports_IDFM/staging/stg_liste_transporteurs.sql
@@ -1,3 +1,18 @@
+SELECT
+    operatorname       AS transporteur_name,
+    operatorref        AS transporteur_ref,
+    housenumber,
+    street,
+    addressline1,
+    town,
+    postcode,
+    postcodeextension,
+    phone,
+    url,
+    furtherdetails,
+    contactperson,
+    logo,
+    email
+FROM {{ source('idfm', 'liste_transporteurs') }}
 
-select *
-from {{source('idfm', 'liste_transporteurs')}}
+
