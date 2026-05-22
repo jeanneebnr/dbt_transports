81e6f8a030905cd998371437b265a5fae7d8aa1b
Jeanne Eichelbrenner <jeanne.eichelbrenner@student.lacapsule.academy>
2026-05-22 07:24:22 +0000

    sgt_accessibilité modif level_id

--- models/transports_IDFM/staging/stg_accessibilite_en_gare.sql
+++ models/transports_IDFM/staging/stg_accessibilite_en_gare.sql
@@ -21,7 +21,7 @@ clean_data AS (
         cast(id_stop_point AS string) AS id_stop_IDFM,
         cast(stop_name AS string)     AS libelle_arret,
         coalesce(cast(accessibility_level_name as string), 'non renseigné') as niveau_accessibilite,
-         coalesce(cast(accessibility_level_id as string), 'non renseigné') as note_accessibilite,
+        coalesce(cast(accessibility_level_id as string), 'non renseigné') as note_accessibilite,
         cast(lat AS float64) AS latitude,
         cast(lon AS float64) AS longitude
     FROM deduplicated
