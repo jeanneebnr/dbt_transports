f447a5667b19c9b0b6efd9e3b214a6d0891c0eaf
lgusermamie@gmail.com <lgusermamie@gmail.com>
2026-05-22 09:57:40 +0000

    les dims stifs opérationnelle

--- models/transports_IDFM/marts/dim_arrets_stif.sql
+++ models/transports_IDFM/marts/dim_arrets_stif.sql
@@ -1,10 +1,27 @@
 {{config(materialized='table')}}
-SELECT 
-id_transporteur_stif,
-id_reseau_stif,
-libelle_arret,
-id_zone_arret
+SELECT DISTINCT
+    id_transporteur_stif,
+    id_reseau_stif,
+    libelle_arret,
+    id_zone_arret
 
-from {{ref('stg_nb_fer_2023_s1')}}  al
+FROM (
+    SELECT id_transporteur_stif, id_reseau_stif, libelle_arret, id_zone_arret
+    FROM {{ ref('stg_nb_fer_2023_s1') }}
 
+    UNION ALL
+
+    SELECT id_transporteur_stif, id_reseau_stif, libelle_arret, id_zone_arret
+    FROM {{ ref('stg_nb_fer_2023_s2') }}
+
+    UNION ALL
+
+    SELECT id_transporteur_stif, id_reseau_stif, libelle_arret, id_zone_arret
+    FROM {{ ref('stg_nb_fer_2024_s1') }}
+
+    UNION ALL
+
+    SELECT id_transporteur_stif, id_reseau_stif, libelle_arret, id_zone_arret
+    FROM {{ ref('stg_nb_fer_2024_t3') }}
+)
 
--- models/transports_IDFM/marts/dim_lignes_stif.sql
+++ models/transports_IDFM/marts/dim_lignes_stif.sql
@@ -1,11 +1,80 @@
 {{config(materialized='table')}}
 
+SELECT DISTINCT
+    id_transporteur_stif,
+    id_reseau_stif,
+    id_ligne_stif,
+    libelle_ligne,
+    id_groupoflines
 
-select 
-id_transporteur_stif,
-id_reseau_stif,
-id_ligne_stif,
-libelle_ligne,
-id_groupoflines
+FROM (
 
-from {{ref('stg_nb_surface_2023_t1')}}
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2023_t1') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2023_t2') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2023_t3') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2023_t4') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2024_t1') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2024_t2') }}
+
+    UNION ALL
+
+    SELECT
+        id_transporteur_stif,
+        id_reseau_stif,
+        id_ligne_stif,
+        libelle_ligne,
+        id_groupoflines
+    FROM {{ ref('stg_nb_surface_2024_t3') }}
+
+)
