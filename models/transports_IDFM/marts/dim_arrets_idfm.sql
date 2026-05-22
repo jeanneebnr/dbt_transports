2a33b3aa430992ce58a16b8105178299aceef963
RUDY TRIPIER <rudy.tripier@student.lacapsule.academy>
2026-05-22 15:15:04 +0000

    Fix test 1

--- models/transports_IDFM/marts/dim_arrets_idfm.sql
+++ models/transports_IDFM/marts/dim_arrets_idfm.sql
@@ -1,14 +1,28 @@
-{{config(materialized='table')}}
-SELECT 
-al.id_stop_idfm,
-al.libelle_arret,
-al.longitude,
-al.latitude,
-a.niveau_accessibilite,
-a.note_accessibilite,
-al.ville,
-al.code_postal
+{{ config(materialized='table') }}
 
-from {{ref('stg_arrets_lignes')}}  al
-left join  {{ref('stg_accessibilite_en_gare')}}  a using (id_stop_IDFM)
+SELECT
+    id_stop_idfm,
+    libelle_arret,
+    longitude,
+    latitude,
+    niveau_accessibilite,
+    note_accessibilite,
+    ville,
+    code_postal
 
+FROM (
+    SELECT
+        al.id_stop_idfm,
+        al.libelle_arret,
+        al.longitude,
+        al.latitude,
+        a.niveau_accessibilite,
+        a.note_accessibilite,
+        al.ville,
+        al.code_postal,
+        ROW_NUMBER() OVER (PARTITION BY al.id_stop_idfm ORDER BY al.id_stop_idfm) AS row_num
+    FROM {{ ref('stg_arrets_lignes') }} AS al
+    LEFT JOIN {{ ref('stg_accessibilite_en_gare') }} AS a
+        USING (id_stop_idfm)
+) AS ranked
+WHERE row_num = 1
