9aff91eb218403b5d0820def2c7920598299b00c
Jeanne Eichelbrenner <jeanne.eichelbrenner@student.lacapsule.academy>
2026-05-25 09:42:58 +0000

    int_arrets_idfm enrichi avec id_zdc

--- models/transports_IDFM/intermediate/int_arrets_idfm.sql
+++ models/transports_IDFM/intermediate/int_arrets_idfm.sql
@@ -1,60 +1,62 @@
-{{ config(materialized='table') }}
-
 WITH arrets_lignes AS (
 
     SELECT
-        id_stop_idfm    AS id_arret_idfm,
-        libelle_arret,
-        ville,
-        code_postal,
-        latitude,
-        longitude,
-        type_transport,
-        reservation
-    FROM {{ ref('stg_arrets_lignes') }}
+        al.id_stop_idfm AS id_arret_idfm,
+        ref.id_zdc,
+
+        al.libelle_arret,
+        al.ville,
+        al.code_postal,
+
+        al.latitude,
+        al.longitude,
+
+        al.type_transport,
+        al.reservation
+
+    FROM {{ ref('stg_arrets_lignes') }} al
+
+    LEFT JOIN {{ ref('stg_arrets_referentiel') }} ref
+        ON al.id_stop_idfm = ref.id_arret_idfm
 
 ),
 
 accessibilite AS (
 
     SELECT
-        id_stop_idfm        AS id_arret_idfm,
+        id_stop_idfm AS id_arret_idfm,
+
         niveau_accessibilite,
-        note_accessibilite,
-        latitude,
-        longitude
+        note_accessibilite
+
     FROM {{ ref('stg_accessibilite_en_gare') }}
-
-),
-
-joined AS (
-
-    SELECT
-        al.id_arret_idfm,
-        al.libelle_arret,
-        al.ville,
-        al.code_postal,
-        COALESCE(al.latitude, acc.latitude)   AS latitude,
-        COALESCE(al.longitude, acc.longitude) AS longitude,
-        al.type_transport,
-        al.reservation,
-        acc.niveau_accessibilite,
-        acc.note_accessibilite
-    FROM arrets_lignes al
-    LEFT JOIN accessibilite acc ON al.id_arret_idfm = acc.id_arret_idfm
 
 )
 
 SELECT
-    id_arret_idfm,
-    libelle_arret,
-    ville,
-    code_postal,
-    latitude,
-    longitude,
-    type_transport,
-    reservation,
-    niveau_accessibilite,
-    note_accessibilite
-FROM joined
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id_arret_idfm ORDER BY LENGTH(libelle_arret) DESC) = 1
+
+    al.id_arret_idfm,
+    al.id_zdc,
+
+    al.libelle_arret,
+    al.ville,
+    al.code_postal,
+
+    al.latitude,
+    al.longitude,
+
+    al.type_transport,
+    al.reservation,
+
+    acc.niveau_accessibilite,
+    acc.note_accessibilite
+
+FROM arrets_lignes al
+
+LEFT JOIN accessibilite acc
+    ON al.id_arret_idfm = acc.id_arret_idfm
+
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY al.id_zdc
+    ORDER BY LENGTH(al.libelle_arret) DESC
+) = 1
