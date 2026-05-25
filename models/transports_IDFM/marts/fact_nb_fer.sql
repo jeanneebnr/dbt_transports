2ee2dcb333a62541da448a4a00852113b813d855
ines duflos <lgusermamie@gmail.com>
2026-05-25 16:28:13 +0000

    fact_nb_fer_ok

--- models/transports_IDFM/marts/fact_nb_fer.sql
+++ models/transports_IDFM/marts/fact_nb_fer.sql
@@ -27,9 +27,8 @@ WITH source AS (
 
 SELECT
     d.date,
-    a.id_arret,
     nf.id_zone_arret,
-    a.libelle_arret,
+    nf.libelle_arret,
     t.titre,
     nf.validations_nb
 
@@ -37,6 +36,6 @@ FROM source  nf
 
 JOIN {{ ref('dim_categorie_titres') }} t
     ON nf.categorie_titre = t.titre
-Left JOIN {{ ref('dim_arrets_zdc') }} a on cast(a.id_arret as string ) = cast(nf.id_zone_arret as string)
+left JOIN {{ ref('dim_arrets_zdc') }} a on cast(a.id_arret as string ) = cast(nf.id_zone_arret as string)
 JOIN {{ ref('dim_date') }} d
     USING (date)
