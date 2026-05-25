9077a134ebcfde82ac0e533061a5d6e2ed9879b2
ines duflos <lgusermamie@gmail.com>
2026-05-25 15:38:01 +0000

    recup table arret

--- models/transports_IDFM/marts/fact_profil_fer.sql
+++ models/transports_IDFM/marts/fact_profil_fer.sql
@@ -1,25 +1,38 @@
-{{config(materialized='table')}}
-SELECT DISTINCT
-    id_zone_arret,
-    categorie_jour,
-    heure,
-    validations_pct
+{{ config(materialized='table') }}
 
-FROM (
-    SELECT id_zone_arret,categorie_jour,heure,validations_pct
+WITH source AS (
+
+    SELECT *
     FROM {{ ref('stg_profil_fer_2024_s1') }}
-     
+
     UNION ALL
 
-    SELECT id_zone_arret,categorie_jour,heure,validations_pct
+    SELECT *
     FROM {{ ref('stg_profil_fer_2024_t3') }}
 
     UNION ALL
 
-    SELECT id_zone_arret,categorie_jour,heure,validations_pct
+    SELECT *
     FROM {{ ref('stg_profil_fer_2023_s1') }}
+
     UNION ALL
 
-    SELECT id_zone_arret,categorie_jour,heure,validations_pct
+    SELECT *
     FROM {{ ref('stg_profil_fer_2023_s2') }}
+
 )
+
+SELECT DISTINCT
+    dl.id_ligne,
+    dl.privatecode,
+    nf.libelle_arret,
+    nf.categorie_jour,
+    nf.heure,
+    nf.validations_pct
+
+FROM source nf
+
+JOIN {{ ref('dim_lignes') }} dl
+ON CONCAT(
+    TRIM(CAST(nf.id_transporteur_stif AS STRING)),TRIM(CAST(nf.id_reseau_stif AS STRING)),
+    TRIM(CAST(nf.id_arret_stif AS STRING))) = TRIM(dl.privatecode)
