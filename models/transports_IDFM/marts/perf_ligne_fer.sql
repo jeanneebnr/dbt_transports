a386c2447ea54bf45ac0feca033efb0ffe8b4e0a
ines duflos <lgusermamie@gmail.com>
2026-06-15 15:47:32 +0000

    perf_lignes ok

---
+++ models/transports_IDFM/marts/perf_ligne_fer.sql
@@ -0,0 +1,91 @@
+{{ config(materialized='table') }}
+
+WITH 
+validations_jour AS (
+ 
+    SELECT
+        date,
+        id_arret,
+     
+        SUM(validations_nb) AS total_validations_jour,
+        CASE
+            WHEN EXTRACT(YEAR FROM date) = 2023 THEN
+                CONCAT('2023_s', CAST(CASE WHEN EXTRACT(MONTH FROM date) <= 6 THEN 1 ELSE 2 END AS STRING))
+            WHEN EXTRACT(YEAR FROM date) = 2024 THEN
+                CASE
+                    WHEN EXTRACT(MONTH FROM date) <= 6 THEN '2024_s1'
+                    WHEN EXTRACT(MONTH FROM date) <= 9 THEN '2024_t3'
+                    ELSE NULL
+                END
+            ELSE NULL
+        END AS periode
+ 
+    FROM {{ ref('fact_nb_fer') }}
+    GROUP BY date, id_arret
+ 
+),
+ 
+trafic_journalier AS (
+ 
+    SELECT
+        date,
+        id_ligne,
+        tranche_horaire,
+        freq_theo,
+        freq_reel,
+        taux_service_pct,
+        retard_moyen_minute,
+        facteur_retard,
+        incident_detecte,
+        incident_type
+ 
+    FROM {{ ref('fact_trafic_heure') }}
+ 
+),
+ 
+dim_date_cte AS (
+ 
+    SELECT * FROM {{ ref('dim_date') }}
+ 
+),
+ 
+p AS (
+ 
+    SELECT * FROM {{ ref('fact_profil_fer') }}
+ 
+)
+
+
+ 
+SELECT
+    v.date,
+    da.id_arret,
+    
+    v.total_validations_jour,
+    v.periode,
+    dd.categorie_jour,
+    da.libelle_arret,
+    t.* EXCEPT(date, id_ligne),
+    p.heure,
+    p.validations_pct,
+    cast (round(v.total_validations_jour * p.validations_pct) as int64) AS validations_estimees_heure,
+
+ 
+FROM validations_jour v
+JOIN {{ ref('dim_arrets_zdc') }} da
+    ON v.id_arret = da.id_arret
+LEFT JOIN {{ ref('dim_arrets_lignes') }} cal
+    ON SAFE_CAST(da.id_zdc AS STRING) = SAFE_CAST(cal.id_arret_zdc AS STRING)
+LEFT JOIN trafic_journalier t
+    ON SAFE_CAST(cal.id_ligne AS STRING) = SAFE_CAST(t.id_ligne AS STRING)
+    AND v.date = t.date
+LEFT JOIN dim_date_cte dd
+    ON dd.date = v.date
+LEFT JOIN p
+    ON v.id_arret = p.id_arret
+    AND v.periode = p.periode
+    AND dd.categorie_jour = p.categorie_jour
+
+
+ORDER BY v.periode, v.date
+ 
---
+++ models/transports_IDFM/marts/perf_ligne_surface.sql
@@ -0,0 +1,72 @@
+{{ config(materialized='table') }}
+
+WITH validations_jour AS (
+ 
+    SELECT
+        date,
+        id_ligne,
+        private_code,
+     
+        SUM(validations_nb) AS total_validations_jour,
+        CASE
+            WHEN EXTRACT(YEAR FROM date) = 2023 THEN
+                CONCAT('2023_s', CAST(CASE WHEN EXTRACT(MONTH FROM date) <= 6 THEN 1 ELSE 2 END AS STRING))
+            WHEN EXTRACT(YEAR FROM date) = 2024 THEN
+                CASE
+                    WHEN EXTRACT(MONTH FROM date) <= 6 THEN '2024_s1'
+                    WHEN EXTRACT(MONTH FROM date) <= 9 THEN '2024_t3'
+                    ELSE NULL
+                END
+            ELSE NULL
+        END AS periode
+ 
+    FROM {{ ref('fact_nb_surface') }}
+    GROUP BY date, id_ligne,private_code
+ 
+),
+ 
+
+ 
+dim_date_cte AS (
+ 
+    SELECT * FROM {{ ref('dim_date') }}
+ 
+),
+ 
+p AS (
+ 
+    SELECT * FROM {{ ref('fact_profil_surface') }}
+ 
+)
+
+
+ 
+SELECT
+    v.date,
+    v.id_ligne,
+    d.libelle_ligne,  
+    v.total_validations_jour,
+    v.periode,
+    dd.categorie_jour,
+      
+    p.heure,
+    p.validations_pct,
+    cast(round (v.total_validations_jour * p.validations_pct) as int64) AS validations_estimees_heure
+    
+ 
+FROM validations_jour v
+LEFT JOIN dim_date_cte dd
+    ON dd.date = v.date
+
+Left join {{ref('dim_arrets_zdc')}}  da
+    on safe_cast(v.private_code as string )= safe_cast(da.private_code_arret as string)
+LEFT JOIN p
+    ON safe_cast (p.private_code as string) = SAFE_CAST(v.private_code AS STRING)
+    AND v.periode = p.periode
+    AND dd.categorie_jour = p.categorie_jour
+
+Left join {{ref('dim_lignes')}}  d
+on safe_cast(d.private_code as string) = safe_cast(v.private_code as string)
+ 
+ORDER BY v.periode, v.date
+ 
